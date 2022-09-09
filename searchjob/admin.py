ea0743180c7637129886ae15ea1fe6c514a03a4c
Styazhko <styazhko97@gmail.com>
2022-09-09 21:42:18 +0300

    add models in admin panel

--- searchjob/admin.py
+++ searchjob/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Vacancy)
+admin.site.register(Specialty)
+admin.site.register(Company)
+admin.site.register(Response)
